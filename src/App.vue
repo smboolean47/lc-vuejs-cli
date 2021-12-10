87ccb99db4ee29e5a9febb9791da1585d6db3798
SM Boolean <samuelemadrigali1991@gmail.com>
2021-12-10 13:13:23 +0100

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <Title/>
+    <HelloWorld :msg="ciao"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
+    Title,
     HelloWorld
   }
 }
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<!-- parte html -->
+		<h2>Ciao Classe 47!</h2>
+	</div>
+</template>
+
+<script>
+	// logica del componente
+	export default {
+		name: 'Title'
+	}
+</script>
+
+<style scoped>
+/* Parte di stile */
+h2 {
+	color: pink;
+}
+</style>
